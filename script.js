790af67fc7739c243ea917e4508238a7e8a112de
dighadesarita <149462427+dighadesarita@users.noreply.github.com>
2023-11-02 14:47:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const signupButton = document.getElementById("signup-button");
+
+    signupButton.addEventListener("click", function () {
+        // Implement your signup logic here
+        alert("Sign Up clicked. Implement your logic here.");
+    });
+});
+
